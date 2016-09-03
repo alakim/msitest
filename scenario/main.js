7bdbef0bab136e7af25d6e02ee098e6dcd3df03e
alakim <alakm@rambler.ru>
2016-09-03 13:50:13 +0300

    Загрузка данных

--- scenario/main.js
+++ scenario/main.js
@@ -15,7 +15,34 @@
 		}})())
 		.find(".btLoadJSON").click(function(){
 			$(".pnlMain .pnlData").html($H.img({src:"wait.gif"}));
-			$.get("data/ver_gorod_20160726.json", {}, function(res){
+			//$.get("data/ver_gorod_20160726.json", {}, function(res){});
+			$.ajax({ //чтобы локальный файл корректно грузила - должен быть указан правильный mimeType
+			    url: "data/ver_gorod_20160726.json",
+			    dataType: "json", mimeType: "application/json",
+			    success: function(res){
+				var colNames = $D(res)
+					.map(function(x){return $D.keys(x.Cells);})
+					.flat()
+					.index('x|x')
+					.keys()
+					.raw();
+				$(".pnlMain .pnlData").html((function(){with($H){
+					return table({border:1, cellpadding:3, cellspacing:0},
+						tr(
+							apply(colNames, function(nm){
+								return th(nm);
+							})
+						),
+						apply(res, function(el, i){
+							return tr(
+								apply(colNames, function(nm){
+									return td(el.Cells[nm])
+								})
+							)
+						})
+					);
+				}})());
+			    }
 			});
 		}).end();
 	});
