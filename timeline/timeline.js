91bbda10f7d32a8e7a78c7b8f73f19f56ba44413
alakim <alakm@rambler.ru>
2016-09-18 21:56:26 +0300

    Basic resize

--- timeline/timeline.js
+++ timeline/timeline.js
@@ -4,11 +4,50 @@ var Timeline = (function($D){
 		objectHeight:22
 	};
 	
+	var scenario,
+		svg,
+		timeSpan,
+		timeResolution;
+
 	function drawObject(s, obj){
 		//console.log("drawing ", obj);
-		var pos = {x:10*obj.time, y:obj.usr.y};
-		var objView = s.rect(pos.x, pos.y, 20*obj.duration, Settings.objectHeight)
+		var pos = {x:timeResolution*(obj.time-timeSpan.min), y:obj.usr.y, w:timeResolution*obj.duration};
+		var viewBody = s.rect(pos.x, pos.y, pos.w, Settings.objectHeight)
 			.attr({fill:"#ffc", stroke:"#440"})
+		var name = s.text(pos.x, pos.y - Settings.objectHeight*.2, obj.name);
+		var address = s.text(pos.x, pos.y + Settings.objectHeight*1.6, obj.address);
+		var ctrlW = Settings.objectHeight*1.6;
+		var control = s.rect(pos.x+pos.w - ctrlW, pos.y, ctrlW, Settings.objectHeight)
+			.attr({fill:"#cc8", stroke:"#440"})
+			.drag(
+				function(dx, dy, x, y, e){
+					this.attr({
+						transform: this.data('origTransform') + (this.data('origTransform') ? "T" : "t") + [dx, dy*0]
+					});
+					var bw = +viewBody.attr("width");
+					var diff = x - this.data("x0");
+					var pos = this.data("curPos")+dx;
+
+					//viewBody.attr({width: bw+diff});
+					viewBody.attr({width:pos - viewBody.attr("x")});
+					// console.log(bw, dx, bw+dx, diff, bw+diff);
+					e.stopPropagation();
+				},
+				function(x, y, e){
+					this.data('origTransform', this.transform().local );
+					this.data("x0", x);
+					this.data("curPos", +this.attr("x"));
+					e.stopPropagation();
+				},
+				function(e){
+					// console.log(e);
+					var pos = {x:e.layerX, y:e.layerY};
+					obj.usr = pos;
+					e.stopPropagation();
+				}
+			);
+		var objView = s.g(viewBody, name, address, control)
+			.attr({cursor:"default"})
 			.drag(
 				function(dx, dy){
 					this.attr({
@@ -17,19 +56,50 @@ var Timeline = (function($D){
 				},
 				function(){
 					this.data('origTransform', this.transform().local );
+				},
+				function(e){
+					// console.log(e);
+					var pos = {x:e.layerX, y:e.layerY};
+					obj.usr = pos;
 				}
 			);
 	}
+
+	function getTimeSpan(scen){
+		var tSpan = $D.aggregate(scen, {min:-1, max:0}, function(obj, aggr){
+			if(aggr.min<0) aggr.min = obj.time;
+			else if(aggr.min>obj.time) aggr.min = obj.time;
+			var t2 = obj.time + obj.duration;
+			if(aggr.max<t2) aggr.max = t2;
+			return aggr;
+		});
+		tSpan.duration = tSpan.max - tSpan.min;
+		return tSpan;
+	}
 	
-	function init(scenario){
-		var s = Snap("#pnlTimeline");
+	function init(scen){
+		scenario = scen;
+		svg = Snap("#pnlTimeline");
+		timeSpan = getTimeSpan(scenario);
+		// console.log(typeof(svg.node.width.baseVal.value), svg);
+		timeResolution = svg.node.width.baseVal.value/timeSpan.duration;
+		// console.log(timeResolution);
+		// console.log(timeSpan);
 		$D.each(scenario, function(obj){
-			drawObject(s, obj);
+			drawObject(svg, obj);
+		});
+	}
+
+	function refresh(){
+		svg.clear();
+		$D.each(scenario, function(obj){
+			drawObject(svg, obj);
 		});
 	}
 
 	return {
-		init:init
+		init:init,
+		refresh: refresh
 	};
 })(JDB.version("3.0.1"));
 
