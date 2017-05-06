45270a00c249d4cbc15ca1d8392f0b31cd3fef15
alakim <alakm@rambler.ru>
2017-05-06 03:43:19 +0300

    SVG animation sample: Floating motion

--- svgAnim/animation1.js
+++ svgAnim/animation1.js
@@ -1,18 +1,37 @@
 (function($){
 	var enableTrace = false;
+	var brownian = 1, floating = 2;
+
 	var maxAnimationTime = 1e3,
 		maxAnimationSteps = 0,
 		speed = 1e-1,
-		size = {w:400, h: 250};
+		size = {w:400, h: 250},
+		animationType = brownian;
+
+	$(function(){
+		var selector = $('#selAnimationType');
+		selector.change(function(){
+			animatedItems = [];
+			switch($(this).val()){
+				case 'brownian': animationType = brownian; break;
+				case 'floating': animationType = floating; break;
+				default: animationType = brownian; break;
+			}
+		});
+	});
 
 	var snp, 
 		animatedItems = [];
 
 	function collectItems(){
 		if(animatedItems.length) return;
+
 		var coll = snp.selectAll('.icon');
 		for(var el,i=0; el=coll[i],i<coll.length; i++){
-			animatedItems.push(new AnimatedItem(el));
+			animatedItems.push(
+				animationType==floating? new FloatingItem(el)
+					:new BrownianItem(el)
+			);
 		}
 	}
 
@@ -68,19 +87,45 @@
 		return Math.random()*2 - 1;
 	}
 
-	function AnimatedItem(el){
+	function FloatingItem(el){
 		this.svgEl = el;
-		this.tr0 = el.transform();
 		this.x = 0;
 		this.y = 0;
-		this.size = el.getBBox();
+		this.space = {x:30, y:30}
+		
+		function newDirection(){
+			var angle = Math.PI*2*Math.random();
+			return {x:Math.sin(angle), y:Math.cos(angle)};
+		}
+		this.direction = newDirection();
+
+		this.animate = function(dT){
+			this.x+=dT*speed*this.direction.x;
+			this.y+=dT*speed*this.direction.y;
+
+			if(this.x<-this.space.x 
+				|| this.x>this.space.x
+				|| this.y<-this.space.y
+				|| this.y>this.space.y)
+				this.direction = newDirection();
+
+			var tr1 = [
+				't',
+				this.x,
+				',',
+				this.y
+			].join('');
+			this.svgEl.transform(tr1);
+		};
+	}
+
+	function BrownianItem(el){
+		this.svgEl = el;
+		this.x = 0;
+		this.y = 0;
 		this.space = {x:30, y:30}
 		
 		this.animate = function(dT){
-			this.brownian(dT);
-		};
-		
-		this.brownian = function(dT){
 			this.x += dT*speed*random();
 			if(this.x<-this.space.x) this.x = -this.space.x;
 			else if(this.x>this.space.x) this.x = this.space.x;
